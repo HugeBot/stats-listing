15fa271d138e4fc0bf9eca6cdfb8036add5dd696
Juan Luis Caro <juanluiscarobenito@gmail.com>
2022-06-05 14:58:47 +0000

    Some changes on compile and gitignore

--- compile.sh
+++ compile.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 echo "Compiling for Windows amd64..."
-GOOS=windows GOARCH=amd64 go build -o bin/stats-listing-x86_64.exe main.go
+GOOS=windows GOARCH=amd64 go build -o bin/stats-listing-win_amd64.exe main.go
 
 echo "Compiling for Windows i386..."
-GOOS=windows GOARCH=386 go build -o bin/stats-listing-i386.exe main.go
+GOOS=windows GOARCH=386 go build -o bin/stats-listing-win_i386.exe main.go
 
 echo "Compiling for Darwin amd64..."
-GOOS=darwin GOARCH=amd64 go build -o bin/stats-listing-x86_64-darwin main.go
+GOOS=darwin GOARCH=amd64 go build -o bin/stats-listing-macos_amd64 main.go
 
 echo "Compiling for Linux amd64..."
-GOOS=linux GOARCH=amd64 go build -o bin/stats-listing-x86_64-linux main.go
+GOOS=linux GOARCH=amd64 go build -o bin/stats-listing-linux_amd64 main.go
 
 echo "Compiling for Linux i386..."
-GOOS=linux GOARCH=386 go build -o bin/stats-listing-i386-linux main.go
+GOOS=linux GOARCH=386 go build -o bin/stats-listing-linux_i386 main.go
 
 echo "Done!!"
